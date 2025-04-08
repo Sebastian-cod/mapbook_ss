97fbd9be967e958e64e3233b38041cd41f499f00
Sebastian-cod <u64817@student.wat.edu.pl>
2025-04-08 19:01:01 +0200

    chore: mvt pattern

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
+from utils.controller import get_user_info
+from utils.model import users
 
-
-
-users=[
-    {"name":"Sebastian","location":"Pisz","posts":500},
-    {"name":"Michal","location":"Krasnystaw","posts":200},
-    {"name":"Krzysztof","location":"Poznań","posts":100},
-    {"name":"Bartosz","location":"Ostroleka","posts":300},
-
-
-
-
-]
+def main():
+    get_user_info(users)
 
 
 
 
-def get_user_info(users_data: list)->None:
-    for user in users:
-        print(f"Twój znajomy {user['name']}, z miejscowości: {user['location']} opublikowal {user['posts']} postów)")
-
-        get_user_info(users)
+if __name__ == "__main__":
+    main()
---
+++ utils/model.py
@@ -0,0 +1,7 @@
+users=[
+    {"name":"Sebastian","location":"Pisz","posts":500},
+    {"name":"Michal","location":"Krasnystaw","posts":200},
+    {"name":"Krzysztof","location":"Poznań","posts":100},
+    {"name":"Bartosz","location":"Ostroleka","posts":300},
+
+]
