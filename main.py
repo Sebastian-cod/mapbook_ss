3e7b2df7870f5dd8e1852a1e8720a80f3a34c953
Sebastian-cod <u64817@student.wat.edu.pl>
2025-05-11 21:57:05 +0200

    wip: prepared menu

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-from utils.controller import get_user_info
+
+
 from utils.model import users
+from utils.controller import get_user_info, add_users, remove_user
+
 
 def main():
     while True:
-        print("==============MENU==============")
-        print("0 - zamknij aplikacje")
-        print("1 - wyświetl co u znajomych")
-        print("2 - dodaj nowego użytkownika")
-        print("3 - usuń użytkownika")
-        print("4 - edytuj użytkownika")
-        print("==============MENU==============")
 
-        choice = input('wybierz opcje menu')
-        if choice == '0':
-            break
-        if choice == '1':
-            get_user_info(users)
-
-
-
+        print("==========MENU==========")
+        print("0 - zakończ program")
+        print("1 - pokaż co u znajomych")
+        print("2 - dodaj nowego znajomego")
+        print("3 - usuń znajomego")
+        print('========================')
+        choice = input("wybierz opcje menu: ")
+        if  choice == "0": break
+        if  choice == "1": get_user_info(users)
+        if choice == "2": add_users(users)
+        if choice == "3": remove_user(users)
 
 if __name__ == "__main__":
     main()
