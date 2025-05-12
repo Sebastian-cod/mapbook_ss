1e6b027c09aa60886fc3834bcf63b30b29817780
Sebastian-cod <u64817@student.wat.edu.pl>
2025-05-12 09:30:43 +0200

    wip: gui

---
+++ main.py
@@ -0,0 +1,89 @@
+from tkinter import *
+
+
+
+root = Tk()
+
+root.title('Mapbook_ss')
+root.geometry('1200x700')
+
+ramka_lista_obiektow = Frame(root)
+ramka_formularz = Frame(root)
+ramka_szczegoly_obiektow = Frame(root)
+
+ramka_lista_obiektow.grid(row=0, column=0, padx=50)
+ramka_formularz.grid(row=0, column=1)
+ramka_szczegoly_obiektow.grid(row=1, column=0)
+
+#  ramka_lista obiektow
+
+label_lista_obiektow = Label(ramka_lista_obiektow, text='Lista znajomych')
+label_lista_obiektow.grid(row=0, column=0)
+
+Listbox_lista_obiektow = Listbox(ramka_lista_obiektow)
+Listbox_lista_obiektow.grid(row=1, column=0, columnspan=3)
+
+button_poka_szczegoly=Button(ramka_lista_obiektow,text='Pokaż szczegoly')
+button_poka_szczegoly.grid(row=3, column=0)
+
+
+button_usun_obiekt=Button(ramka_lista_obiektow,text='Usun obiekt')
+button_usun_obiekt.grid(row=3, column=1)
+
+button_edytuj_obiekt=Button(ramka_lista_obiektow,text='Edytuj obiekt')
+button_edytuj_obiekt.grid(row=3, column=2)
+
+#ramka_formularz
+
+label_formularz=Label(ramka_formularz, text='Formularz')
+label_formularz.grid(row=0, column=1, columnspan=2)
+
+
+label_imie=Label(ramka_formularz, text='Imie')
+label_imie.grid(row=1, column=0,sticky=W)
+
+label_nazwisko=Label(ramka_formularz, text='Nazwisko')
+label_nazwisko.grid(row=2, column=0,sticky=W)
+
+label_miejscowosc=Label(ramka_formularz, text='Miejscowość')
+label_miejscowosc.grid(row=3, column=0,sticky=W)
+
+label_liczba_postow=Label(ramka_formularz, text='Liczba postów')
+label_liczba_postow.grid(row=4, column=0,sticky=W)
+
+entry_imie=Entry(ramka_formularz)
+entry_imie.grid(row=1, column=1)
+entry_nazwisko=Entry(ramka_formularz)
+entry_nazwisko.grid(row=2, column=1)
+entry_miejscowosc=Entry(ramka_formularz)
+entry_miejscowosc.grid(row=3, column=1)
+entry_liczba_postow=Entry(ramka_formularz)
+entry_liczba_postow.grid(row=4, column=1)
+
+button_dodaj_obiekt=Button(ramka_formularz, text='Dodaj')
+button_dodaj_obiekt.grid(row=5, column=0, columnspan=2)
+
+#ramka_szczegoly_obiektu
+
+label_szczegoly_obiektow=Label(ramka_szczegoly_obiektow, text='Szczególy użytkownika')
+label_szczegoly_obiektow.grid(row=0, column=0)
+
+label_imie_szczegoly_obiektu=Label(ramka_szczegoly_obiektow, text='Imie: ')
+label_imie_szczegoly_obiektu.grid(row=1, column=0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
